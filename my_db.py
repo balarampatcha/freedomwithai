2aa9a8567e23cdd095af134cc3101b54868fb1f7
Balarampatcha <balarampatcha@gmail.com>
2024-06-21 11:16:54 +0530

    changes in code files

--- my_db.py
+++ my_db.py
@@ -1,37 +1,28 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import pandas as pd
 import os
-from dotenv import load_dotenv
-import docx2txt
-
-load_dotenv()
-api_key=os.getenv("GOOGLE_API_KEY")
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-    chunks = text_splitter.split_text(text)
-    return chunks
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import TextLoader
 
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001",google_api_key=api_key)
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+def get_text_chunks(text_documents):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=750, chunk_overlap=200)
+    documents = text_splitter.split_documents(text_documents)
+    return documents
+
+
+def get_vector_store(documents):
+    embeddings =  OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
+    vector_store = FAISS.from_documents(documents,embeddings)
     vector_store.save_local("faiss_index")
 
-# df=pd.read_excel('data_trail.xlsx')
+    
+loader=TextLoader("main_document.txt")
 
-import pandas as pd
+text_documents=loader.load()
 
-my_text = docx2txt.process("masterclass2.docx")
-
-# df['text']=df['text'].apply(lambda x: x.replace('\n',' '))
-# df['text']=df['text'].apply(lambda x: x.replace('\t',' '))
-# df['text']=df['text'].apply(lambda x: x.replace('\r',' '))
-
-chunks= get_text_chunks(my_text)
+chunks= get_text_chunks(text_documents)
 
 get_vector_store(chunks)
